b6f786857be13d142f7a478dde4c067cf0058b3f
Кристина Алмазова <kristinaalmazova@MacBook-Air-Kristina.local>
2022-09-21 20:36:33 +0300

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите первое число:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число:");
+int numC = Convert.ToInt32(Console.ReadLine());
+int Max = 0;
+if (numA>numB)
+{
+     Max = numA;
+}
+ else
+ {
+   Max = numB;  
+ }
+
+if (Max>numC)
+{
+  Console.WriteLine($"{Max} ");  
+  
+}
+  else
+ {
+   Console.WriteLine($"{numC} ");
+ }
+
