31fd399db801d81e9bdaf175c7ea00d9169ddedb
Кристина Алмазова <kristinaalmazova@MacBook-Air-Kristina.local>
2022-09-21 19:44:46 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите первое число:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numA>numB)
+{
+  Console.WriteLine($"Число {numA} больше числа {numB} ");  
+}
+Console.WriteLine($"Число {numB} больше числа {numA} "); 
