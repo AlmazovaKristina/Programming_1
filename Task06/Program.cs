9a3314bfa35c15f0392fe3f8b3dd986b337b9b9b
Кристина Алмазова <kristinaalmazova@MacBook-Air-Kristina.local>
2022-09-21 21:12:54 +0300

    Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int remainder = num % 2;
+if (remainder == 0)
+{
+  Console.WriteLine("Чётное");  
+}
+  else
+ {
+   Console.WriteLine("Нечётное");
+ }
